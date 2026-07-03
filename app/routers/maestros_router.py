b996bb10ed3adb783b77470e83cc724e470f7b22
Leoncio R. Barrios H. <leonciobarriosh@gmail.com>
2026-07-03 17:54:05 -0500

    Ajustando Docs String para la documentación de l API 7

--- app/routers/maestros_router.py
+++ app/routers/maestros_router.py
@@ -17,7 +17,7 @@ from app.models.maestros_models import (
 router = APIRouter(prefix="/maestros", tags=["Datos Maestros"])
 
 
-@router.get("/nacionalidades", response_model=Nacionalidad)
+@router.get("/nacionalidades", response_model=list[Nacionalidad])
 async def get_nacionalidades():
 	"""
 	Obtiene el listado de Nacionalidades disponibles.
